f9454ccfe461b93679c83f43a6c0c8b4d8be7577
zhchuanger <2209796387@qq.com>
2019-01-26 09:39:06 +0800

    webpack

---
+++ webpack/src/index.js
@@ -0,0 +1,7 @@
+import css from "./index.css";
+
+let a=10;
+console.log(a);
+
+var odiv1 = document.querySelector("#div1");
+odiv1.innerHTML = "我是赵闯";
---
+++ webpack/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const webpack = require("webpack");
+const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+module.exports = {
+    mode: 'development',//production
+    entry: {
+        "xx": './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                //css-loader：用来将css插入到页面的style标签 style-loader：用来处理css文件中的url()等，url挂在到js中 
+                // use:["style-loader","css-loader"]  
+                use:ExtractTextPlugin.extract({
+                    fallback:"style-loader",
+                    use:"css-loader"
+                })
+            },{
+                test:/\.(jpg|png|gif)$/,
+                use:[
+                    {
+                        loader:"url-loader",
+                        options:{
+                            limit:5000000000,//是把小于500000B的文件打成 Base64的格式，写入css。
+                            outputPath:"img/"
+                        }
+
+                    }
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlPlugin({
+            // minify:{
+            //     removeAttributeQuotes:true
+            // },去掉属性的双引号。 
+            hash:true,//清除缓存
+            template:"./src/test.html",//是要打包的html模 版路径和文件名称
+            filename:"test.html"//使名字一致
+        }),
+        new ExtractTextPlugin("./index.css")//把css分离出来
+    ],
+    devServer: {
+        //设置基本目录结构
+        contentBase: path.resolve(__dirname, 'dist'),
+        //服务器的ip地址，可以使用IP地址也可以使用localhost
+        host: 'localhost', //127.0.1
+        //服务端压缩是否开启
+        compress: true,
+        //配置服务端口号
+        port: 8081,
+        //自动开网页
+        open: true
+    },
+    
+
+}
