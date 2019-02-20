03a5e104ece866382eb89e65591a348d03b7061e
zhchuanger <2209796387@qq.com>
2019-02-20 15:18:53 +0800

    react

--- vuemine.1/src/views/photo/photodetail.vue
+++ vuemine.1/src/views/photo/photodetail.vue
@@ -1,22 +1,42 @@
 <template>
     <div>
         <!-- detail photo -->
-        {{photos[$route.params.index]}}
-        <div class="bg" :style="{background:'url('+ photos[$route.params.index].src +')'}"></div>
-
+        <!-- {{photos[$route.params.index]}} -->
+        <router-link to="/photo" v-if="ishow" > 
+            <v-touch v-on:swipeleft="next()" class="bg" :style="{backgroundImage:'url('+ photos[iNow].src +')'}"></v-touch>
+        </router-link>
 
     </div>
 </template>
 
 <script>
     import {mapState} from 'vuex';
+    import Vue from "vue";
+    import VueTouch from "vue-touch"; 
+    Vue.use(VueTouch, {name: 'v-touch'})
     export default {
+        data() {
+            return {
+                iNow:this.$route.params.index,
+                ishow :false
+            }
+        },
         computed: {
             ...mapState(["photos"])
-            
         },
+        created() {
+            if(this.photos.length==0){
+                this.$router.push("/photo")
 
-        
+            }else{
+                this.ishow = true;
+            }
+        },
+        methods: {
+            next(){
+                this.iNow++;
+            }
+        },
     }
 </script>
 
@@ -27,7 +47,9 @@
     left: 0px;
     bottom: 1rem;
     right: 0px;
-    background: black;
+    background: black no-repeat center;
+    background-size: contain;
+
 }
 
 </style>
